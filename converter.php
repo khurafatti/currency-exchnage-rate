722f2abd19383c7d253f34df6b95c7fecc6a0ad6
khurafatti <40769775+khurafatti@users.noreply.github.com>
2020-04-19 21:56:09 +0530

    First Commit

---
+++ converter.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<h1> CURRENCY EXCHANGE RATE </h1>
+<h2> BY YASH SHARMA</h2>
+<!-- Currency Rates Script - EXCHANGERATEWIDGET.COM -->
+<div style="width:240px; border:1px solid #55A516;">
+<p style="background-color:#55A516;margin:0px; height:24px;text-align:center;color:#FFFFFF; font-weight:bold;font-size:16px;padding-top:2px;">
+<a href="https://www.exchangeratewidget.com/" style="color:#FFFFFF;text-decoration:none;" rel="nofollow">Live Currency Rates</a>
+</p>
+<script type="text/javascript" src="//www.exchangeratewidget.com/converter.php?l=en&f=USD&t=USDINR,&a=1&d=DFDFDF&n=FFFFFF&o=000000&v=9">
+</script>
+</div>
+</html>
